9eafe70dcc5391e55d33e75e702c20d58c80f930
YASH <yasshh1818@gmail.com>
2026-04-09 00:03:49 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -29,9 +29,16 @@ except:
 app = FastAPI(title="AgroAI API")
 
 # ── CORS ──
+origins = [
+    "https://agro-ai-bdu.vercel.app",  # your frontend (IMPORTANT)
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
